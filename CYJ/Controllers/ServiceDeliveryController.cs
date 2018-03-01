bd36375cf5a60f2a5f04e9de22f338467161a795
Wafaa Safar <wafaasafar97@gmail.com>
2018-02-28 23:04:39 -0500

    Wafaa's commit

---
+++ CYJ/Controllers/ServiceDeliveryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CYJ.Controllers
+{
+    public class ServiceDeliveryController : Controller
+    {
+        // GET: ServiceDelivery
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Add a goal
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        //View goals
+        public ActionResult viewGoals()
+        {
+            return View();
+        }
+    }
+}
